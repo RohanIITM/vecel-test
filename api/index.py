bc6e8cd3ee429ee841647f9bcea8f93a84581719
RyugaXhypeR <ryugashyper@gmail.com>
2025-02-02 13:20:36 +0000

    feat: added `middleware`

--- api/index.py
+++ api/index.py
@@ -1,9 +1,17 @@
 import json
 
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api")
