d8ade018952929e81da70c850a46dab4c1cfa537
RyugaXhypeR <ryugashyper@gmail.com>
2025-02-02 13:00:13 +0000

    feat: added `index.py`

---
+++ api/index.py
@@ -0,0 +1,15 @@
+import json
+
+from fastapi import FastAPI, Query
+
+
+app = FastAPI()
+
+
+@app.get("/api")
+def api_get(name: list[str] = Query([])) -> dict[str, list[int]]:
+
+    with open("students.json") as file:
+        student_data = json.load(file)
+
+    return {"marks": [student_data[name] for name in names]}
